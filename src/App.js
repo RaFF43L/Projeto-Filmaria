60acf8cd7318bbf043f872337f658ed5b34bdcc1
Raffael Pimentel Ribeiro <ribeiro_pimentel@discente.ufg.br>
2022-01-24 11:16:06 -0300

    Header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//https://sujeitoprogramador.com/r-api/?api=filmes/
+import './style.css';
+import Routes from "./routes";
+function App() {
+
+  return (
+    <div className="App">
+      <Routes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+//https://sujeitoprogramador.com/r-api/?api=filmes/
+
+function App() {
+
+    return (
+      <div className="App">
+      <h1>Pagina Home</h1>
+      </div>
+    );
+  }
+  
+  export default App;
+  
