48cb7d7a2c80cf0c81d559329e342d565d24b6e4
Raffael Pimentel Ribeiro <ribeiro_pimentel@discente.ufg.br>
2022-01-24 13:51:52 -0300

    HomePage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,36 @@
 //https://sujeitoprogramador.com/r-api/?api=filmes/
+import './style.css';
+import { useEffect, useState } from 'react';
+import reactRouterDom from 'react-router-dom';
+import api from '../../services/api';
+import {Link} from 'react-router-dom';
+function Home() {
+  const [filmes, setFilmes] = useState([]);
 
-function App() {
-
+  useEffect(()=>{
+    async function loadFilmes(){
+      //sujeitoprogramador.com
+      const response = await api.get('r-api/?api=filmes')
+      setFilmes(response.data);
+    }
+    loadFilmes();
+  },[]);
     return (
-      <div className="App">
-      <h1>Pagina Home</h1>
+      <div className='container'>
+        <div className='lista-filmes'>
+          {filmes.map((filme)=>{
+            return(
+              <article key={filme.id}>
+                  <strong>{filme.nome}</strong>
+                  <img src={filme.foto} alt={filme.nome} />
+                  <Link to="/">Acessar</Link>
+              </article>
+            )
+          })}
+        </div>
       </div>
     );
   }
   
-  export default App;
+  export default Home;
   
